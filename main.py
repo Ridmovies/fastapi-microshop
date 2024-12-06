163d2a5d9a547715775cb676e9e1af24f303a08e
riddler_rid <unkownrid@gmail.com>
2024-12-06 15:05:09 +0300

    install sqlalchemy

--- main.py
+++ main.py
@@ -8,6 +8,5 @@ app = FastAPI()
 app.include_router(items_route, prefix="/items")
 app.include_router(users_route, prefix="/users")
 
-
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
