a864860e64e74d76120f307b62076c321a2fc8a5
riddler_rid <unkownrid@gmail.com>
2024-12-06 15:54:53 +0300

    sqlalchemy settings

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
+from core.models.base import Base
+from core.models.db_helper import db_helper
 from items_views import router as items_route
 from users.views import router as users_route
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(items_route, prefix="/items")
 app.include_router(users_route, prefix="/users")
 
