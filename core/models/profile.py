9b6d02a84d3001f316040d20a728070841f86ea8
riddler_rid <unkownrid@gmail.com>
2024-12-19 15:57:20 +0300

    add Profile model

--- core/models/profile.py
+++ core/models/profile.py
@@ -8,8 +8,9 @@ from core.models.mixins import UserRelationMixin
 class Profile(UserRelationMixin, Base):
     _user_id_unique = True
     _user_back_populates = "profile"
-    firstname: Mapped[str] | None = mapped_column(String(32))
-    lastname: Mapped[str] | None = mapped_column(String(32))
-    bio: Mapped[str] | None
+
+    firstname: Mapped[str | None] = mapped_column(String(32))
+    lastname: Mapped[str | None] = mapped_column(String(32))
+    bio: Mapped[str | None]
 
     # user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), unique=True)
